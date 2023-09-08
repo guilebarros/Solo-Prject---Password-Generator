eb029df997afe2ef8520aa5e79652b4a6bb38398
guilebarros <barros.guilherme@gmail.com>
2023-09-08 11:22:51 -0300

    Basic JS Hard Coded Interaction

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L",
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L",
                 "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"]
 
+const buttonGenerator = document.getElementById("button")
+
+const passwordOne = document.getElementById("btn-1")
+const passwordTwo = document.getElementById("btn-2")
 
 
 
+buttonGenerator.addEventListener("click", function() {
+    passwordOne.textContent += "Howdy"
+    passwordTwo.textContent += "Lula Livre"
+
+})
