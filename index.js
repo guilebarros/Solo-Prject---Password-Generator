318bff1933c164de2811dc1294b546d831e65946
guilebarros <barros.guilherme@gmail.com>
2023-09-08 14:44:18 -0300

    Stretch Goals UI implemented

--- index.js
+++ index.js
@@ -2,15 +2,38 @@ const characters =["A","B","C","D","E","F","G","H","I","J","K","L",
                 "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"]
 
+
+
 const buttonGenerator = document.getElementById("button")
 
 const passwordOne = document.getElementById("btn-1")
 const passwordTwo = document.getElementById("btn-2")
 
+const symbolCheck = document.getElementById("symbol-check")
+const numberCheck = document.getElementById("number-check")
 
+
+
+
+symbolCheck.addEventListener('change', function() {
+    if (this.checked) {
+      console.log("Symbol is checked..");
+    } else {
+      console.log("Symbol is not checked..");
+    }
+  });
+
+  numberCheck.addEventListener('change', function() {
+    if (this.checked) {
+      console.log("Numbox is checked..");
+    } else {
+      console.log("Numbox is not checked..");
+    }
+  });
 
 buttonGenerator.addEventListener("click", function() {
     passwordOne.textContent += "Howdy"
     passwordTwo.textContent += "Lula Livre"
 
 })
+
